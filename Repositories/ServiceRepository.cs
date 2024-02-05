58482aa9fe7f52cc02d169effdcd206d14de0444
tnapolitano <tnapolitano@mobydigital.com>
2024-02-05 15:00:00 -0300

    Se creó el ServiceRepository

---
+++ Repositories/ServiceRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        private readonly ManagementServiceContext _context;
+
+        public ServiceRepository(ManagementServiceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Service>> GetServices()
+        {
+            var services = await _context.Services.ToListAsync();
+            return services;
+        }
+    }
+}
