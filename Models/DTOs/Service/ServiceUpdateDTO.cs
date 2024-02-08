31b589c5d49b417e3362cc49b6735da7c385bb06
tnapolitano <tnapolitano@mobydigital.com>
2024-02-08 16:18:35 -0300

    Se crea ServiceUpdateDTO

---
+++ Models/DTOs/Service/ServiceUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Service
+{
+    public class ServiceUpdateDTO
+    {
+        public int ServiceId { get; set; }
+        public int ServiceTypeId { get; set; }
+        public string ServiceName { get; set; } = null!;
+        public double PricePerUnit { get; set; }
+    }
+}
