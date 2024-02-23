8fe55797a215de46af01c4ce9fbef74451cc88bc
tnapolitano <tnapolitano@mobydigital.com>
2024-02-23 13:16:53 -0300

    Se crea DTO para distrito con sus servicios

---
+++ Models/DTOs/District/DistrictWithServicesDTO.cs
@@ -0,0 +1,14 @@
+﻿using Models.DTOs.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.District
+{
+    public class DistrictWithServicesDTO : DistrictDTO
+    {
+        public IList<ServiceDTO> Services { get; set; }
+    }
+}
