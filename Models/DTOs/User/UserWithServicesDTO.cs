e2551486f9fe030a1d616a9a5dc5e3757d9304c1
tnapolitano <tnapolitano@mobydigital.com>
2024-02-27 12:25:25 -0300

    Se crea DTO para usuario con servicios suscritos

--- Models/DTOs/User/UserWithServicesDTO.cs
+++ Models/DTOs/User/UserWithServicesDTO.cs
@@ -3,8 +3,18 @@ using Models.DTOs.Service;
 
 namespace Models.DTOs.User
 {
-    public class UserWithServicesDTO : UserDTO
+    public class UserWithServicesDTO
     {
-        public IList<ServiceDTO> Services { get; set; } = new List<ServiceDTO>();
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        public int AddressId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string DniNumber { get; set; } = null!;
+        public DateTime CreationDate { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Active { get; set; } = null!;
+        public IList<ServiceSubscriptionDTO> ServiceSubscriptions { get; set; } = new List<ServiceSubscriptionDTO>();
     }
 }
