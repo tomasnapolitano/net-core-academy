264d0b265c6ecb9e02eab371f7c7f896891e352f
tnapolitano <tnapolitano@mobydigital.com>
2024-02-27 09:33:28 -0300

    Se crea DTO para retornar usuario con sus servicios suscritos

---
+++ Models/DTOs/User/UserWithServicesDTO.cs
@@ -0,0 +1,10 @@
+﻿using Models.DTOs.District;
+using Models.DTOs.Service;
+
+namespace Models.DTOs.User
+{
+    public class UserWithServicesDTO : UserDTO
+    {
+        public IList<ServiceDTO> Services { get; set; } = new List<ServiceDTO>();
+    }
+}
