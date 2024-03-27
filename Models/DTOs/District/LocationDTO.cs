7287fb8fe6b84de24fffa74bc33e5268029c4974
tnapolitano <tnapolitano@mobydigital.com>
2024-03-27 15:56:49 -0300

    Se crea LocationDTO

---
+++ Models/DTOs/District/LocationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.District
+{
+    public class LocationDTO
+    {
+        public int LocationId { get; set; }
+        public string LocationName { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+
+        public virtual DistrictDTO District { get; set; }
+    }
+}
