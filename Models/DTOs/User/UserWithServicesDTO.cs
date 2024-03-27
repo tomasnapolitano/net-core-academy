19d6bd21129724cdaf1291c01ecf947afadafd74
tnapolitano <tnapolitano@mobydigital.com>
2024-03-27 13:41:08 -0300

    Se crean y modifican DTOs para acomodar a lógica nueva/corregida

--- Models/DTOs/User/UserWithServicesDTO.cs
+++ Models/DTOs/User/UserWithServicesDTO.cs
@@ -12,8 +12,11 @@ namespace Models.DTOs.User
         public string Email { get; set; } = null!;
         public string DniNumber { get; set; } = null!;
         public DateTime CreationDate { get; set; }
-        public string FullName { get; set; } = null!;
         public string Active { get; set; } = null!;
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
         public IList<ServiceSubscriptionDTO> ServiceSubscriptions { get; set; } = new List<ServiceSubscriptionDTO>();
     }
 }
