a066fa51fa8ca4c5eae0ad38e542bbc2828b0a46
tnapolitano <tnapolitano@mobydigital.com>
2024-03-27 13:41:44 -0300

    Se crea nueva excepción custom para servicios no disponibles

---
+++ Utils/Middleware/UnavailableServiceException.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Utils.Middleware
+{
+    public class UnavailableServiceException : Exception
+    {
+        public UnavailableServiceException() : base()
+        {
+        }
+        public UnavailableServiceException(string message) : base(message)
+        {
+        }
+        public UnavailableServiceException(string message, params object[] args) : base(String.Format(CultureInfo.CurrentCulture, message, args))
+        {
+        }
+    }
+}
