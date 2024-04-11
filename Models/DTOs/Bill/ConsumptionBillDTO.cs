a281c60a0dac3cc2a1897ead7d3c5d2ac85428c7
tnapolitano <tnapolitano@mobydigital.com>
2024-04-11 15:56:23 -0300

    Se agrega objeto userDTO y Id a consumptionBillDTO

--- Models/DTOs/Bill/ConsumptionBillDTO.cs
+++ Models/DTOs/Bill/ConsumptionBillDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace Models.DTOs.Bill
+﻿using Models.DTOs.User;
+
+namespace Models.DTOs.Bill
 {
     public class ConsumptionBillDTO
     {
-        public int UserId { get; set; }
+        //public int UserId { get; set; }
+        public int ConsumptionBillId { get; set; }
+        public UserDTO User { get; set; }
         public int BillStatusId { get; set; }
         public DateTime BillDate { get; set; }
         public double Total { get; set; }
