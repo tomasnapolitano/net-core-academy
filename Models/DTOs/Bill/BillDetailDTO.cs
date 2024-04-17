5a465088bf9d5dea3bceff17ddd6fffd329d3d4d
tnapolitano <tnapolitano@mobydigital.com>
2024-04-17 09:25:32 -0300

    Se modifica BillDetailDTO para contener objeto subscription

--- Models/DTOs/Bill/BillDetailDTO.cs
+++ Models/DTOs/Bill/BillDetailDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace Models.DTOs.Bill
+﻿using Models.DTOs.Service;
+
+namespace Models.DTOs.Bill
 {
     public class BillDetailDTO
     {
-        public int SubscriptionId { get; set; }
+        //public int SubscriptionId { get; set; }
+        public ServiceSubscriptionDTO Subscription { get; set; }
         public int ConsumptionBillId { get; set; }
         public double UnitsConsumed { get; set; }
         public int DaysBilled { get; set; }
