14b2241efc29bae9a5874fe49b5dfc199919b248
tnapolitano <tnapolitano@mobydigital.com>
2024-04-17 09:27:47 -0300

    Se quita código comentado

--- Models/DTOs/Bill/BillDetailDTO.cs
+++ Models/DTOs/Bill/BillDetailDTO.cs
@@ -4,7 +4,6 @@ namespace Models.DTOs.Bill
 {
     public class BillDetailDTO
     {
-        //public int SubscriptionId { get; set; }
         public ServiceSubscriptionDTO Subscription { get; set; }
         public int ConsumptionBillId { get; set; }
         public double UnitsConsumed { get; set; }
