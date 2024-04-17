adce893a3470992f022739992925fda91dacc959
tnapolitano <tnapolitano@mobydigital.com>
2024-04-17 14:29:34 -0300

    Se crea BillStatusDTO y se agrega el objeto a ConsumptionBillDTO

---
+++ Models/DTOs/Bill/BillStatusDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Bill
+{
+    public class BillStatusDTO
+    {
+        public int BillStatusId { get; set; }
+        public string BillStatusName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
